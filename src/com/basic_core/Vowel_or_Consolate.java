74252d9a322ca23d6c3033a27ba2a5533d294fd7
Textspot <sachinkatarki1995@gmail.com>
2021-11-01 17:38:58 +0530

    Added:welcome to Vowel and consonant programme

---
+++ src/com/basic_core/Vowel_or_Consolate.java
@@ -0,0 +1,17 @@
+package com.basic_core;
+
+public class Vowel_or_Consolate {
+	
+	public static void main(String[] args) {
+		char alfa = 'a';
+		if (alfa == 'a' || alfa == 'e' || alfa == 'o' || alfa == 'i' || alfa == 'u') {
+			System.out.println("This is a Vowel");
+		} else {
+			System.out.println("this is Consonant");
+		}
+	}
+}
+
+	
+
+
