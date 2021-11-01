e428f669fef0c82d0b0c44a9ce384a551dbcb3bd
Textspot <sachinkatarki1995@gmail.com>
2021-11-01 17:11:53 +0530

    Added:welcome to Factors programme

---
+++ src/com/basic_core/Factors.java
@@ -0,0 +1,18 @@
+package com.basic_core;
+import java.util.Scanner;
+public class Factors {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number");
+		int num = sc.nextInt();
+		sc.close();
+		for (int i = 1; i <= num; i++) {
+			if (num % i == 0) {
+				System.out.println(i);
+			}
+		}
+
+	}
+
+}
